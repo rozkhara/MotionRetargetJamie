23b06356e89742d296c98bed6854ebaaec2d4292
syee19 <syee19@korea.ac.kr>
2022-07-21 12:57:57 +0900

    Add globalBasis flag to create sourceTpose in global

--- Assets/Scripts/TposeAlignment.cs
+++ Assets/Scripts/TposeAlignment.cs
@@ -53,6 +53,8 @@ public class TposeAlignment : MonoBehaviour
 
     public PositionWiseUpdate a;
 
+    public bool globalBasis;
+
     void Start()
     {
         SourceBones = new Bone[(int)SourceBoneIndex.count];
@@ -113,53 +115,82 @@ public class TposeAlignment : MonoBehaviour
         SourceBones[(int)SourceBoneIndex.lLowerArm].BoneLength = Vector3.Distance(a.firstTransform[11], a.firstTransform[12]);
         SourceBones[(int)SourceBoneIndex.lHand].BoneLength = Vector3.Distance(a.firstTransform[12], a.firstTransform[13]);
 
-        //target skeleton의 정보를 받아와서 bone의 orientation 반영 (localPosition을 이용해 자식의 부모기준 위치를 받아옴)
-        SourceBones[(int)SourceBoneIndex.spine].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].GetChild(0).localPosition);
-        SourceBones[(int)SourceBoneIndex.chest].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Spine].GetChild(0).localPosition);
-        SourceBones[(int)SourceBoneIndex.neck].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(5).localPosition);  //원본에서 cape, wing 때문에 face가 5번 index
-        SourceBones[(int)SourceBoneIndex.head].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(5).localPosition);
+        if (globalBasis)
+        {
+            //target skeleton의 정보를 받아와서 bone의 orientation 반영 (localPosition을 이용해 자식의 부모기준 위치를 받아옴)
+            SourceBones[(int)SourceBoneIndex.spine].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].position);
+            SourceBones[(int)SourceBoneIndex.chest].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Spine].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Spine].position);
+            SourceBones[(int)SourceBoneIndex.neck].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(5).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].position);  //원본에서 cape, wing 때문에 face가 5번 index
+            SourceBones[(int)SourceBoneIndex.head].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(5).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].position);
 
-        SourceBones[(int)SourceBoneIndex.rUpperLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].GetChild(2).localPosition);
-        SourceBones[(int)SourceBoneIndex.rLowerLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegR].GetChild(0).localPosition);
-        SourceBones[(int)SourceBoneIndex.rFoot].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_FootR].GetChild(0).localPosition);
+            SourceBones[(int)SourceBoneIndex.rUpperLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].GetChild(2).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].position);
+            SourceBones[(int)SourceBoneIndex.rLowerLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperLegR].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperLegR].position);
+            SourceBones[(int)SourceBoneIndex.rFoot].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegR].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegR].position);
 
-        SourceBones[(int)SourceBoneIndex.lUpperLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].GetChild(1).localPosition);
-        SourceBones[(int)SourceBoneIndex.lLowerLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegL].GetChild(0).localPosition);
-        SourceBones[(int)SourceBoneIndex.lFoot].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_FootL].GetChild(0).localPosition);
+            SourceBones[(int)SourceBoneIndex.lUpperLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].GetChild(1).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].position);
+            SourceBones[(int)SourceBoneIndex.lLowerLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperLegL].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperLegL].position);
+            SourceBones[(int)SourceBoneIndex.lFoot].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegL].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegL].position);
 
-        SourceBones[(int)SourceBoneIndex.rUpperArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(2).localPosition
-                                                                                    - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(1).localPosition);
-        SourceBones[(int)SourceBoneIndex.rLowerArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmR].GetChild(0).localPosition);
-        SourceBones[(int)SourceBoneIndex.rHand].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_HandR].GetChild(0).localPosition);
+            SourceBones[(int)SourceBoneIndex.rUpperArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmR].position
+                                                                                        - t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmL].position);
+            SourceBones[(int)SourceBoneIndex.rLowerArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmR].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmR].position);
+            SourceBones[(int)SourceBoneIndex.rHand].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmR].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmR].position);
 
-        SourceBones[(int)SourceBoneIndex.lUpperArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(1).localPosition 
-                                                                                    - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(2).localPosition);
-        SourceBones[(int)SourceBoneIndex.lLowerArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmL].GetChild(0).localPosition);
-        SourceBones[(int)SourceBoneIndex.lHand].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_HandL].GetChild(0).localPosition);  //target과 source skeleton의 위상이 다르므로, 양 UpperArm은 서로의 좌표를 이용해 구함
+            SourceBones[(int)SourceBoneIndex.lUpperArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmL].position
+                                                                                        - t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmR].position);
+            SourceBones[(int)SourceBoneIndex.lLowerArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmL].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmL].position);
+            SourceBones[(int)SourceBoneIndex.lHand].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmL].GetChild(0).position - t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmL].position);  
+            //target과 source skeleton의 위상이 다르므로, 양 UpperArm은 서로의 좌표를 이용해 구함
+        }
+        else
+        {
+            //target skeleton의 정보를 받아와서 bone의 orientation 반영 (localPosition을 이용해 자식의 부모기준 위치를 받아옴)
+            SourceBones[(int)SourceBoneIndex.spine].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].GetChild(0).localPosition);
+            SourceBones[(int)SourceBoneIndex.chest].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Spine].GetChild(0).localPosition);
+            SourceBones[(int)SourceBoneIndex.neck].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(5).localPosition);  //원본에서 cape, wing 때문에 face가 5번 index
+            SourceBones[(int)SourceBoneIndex.head].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(5).localPosition);
 
+            SourceBones[(int)SourceBoneIndex.rUpperLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].GetChild(2).localPosition);
+            SourceBones[(int)SourceBoneIndex.rLowerLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegR].GetChild(0).localPosition);
+            SourceBones[(int)SourceBoneIndex.rFoot].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_FootR].GetChild(0).localPosition);
 
-        //basis 일치를 위해 rotation 값 가져오기
-        rootNode.rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].rotation;
-        SourceBones[(int)SourceBoneIndex.spine].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_Spine].rotation;
-        SourceBones[(int)SourceBoneIndex.chest].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].rotation;
-        SourceBones[(int)SourceBoneIndex.neck].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Face].rotation;
-        SourceBones[(int)SourceBoneIndex.head].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Face].rotation;
+            SourceBones[(int)SourceBoneIndex.lUpperLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].GetChild(1).localPosition);
+            SourceBones[(int)SourceBoneIndex.lLowerLeg].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegL].GetChild(0).localPosition);
+            SourceBones[(int)SourceBoneIndex.lFoot].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_FootL].GetChild(0).localPosition);
 
-        SourceBones[(int)SourceBoneIndex.rUpperLeg].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperLegR].rotation;
-        SourceBones[(int)SourceBoneIndex.rLowerLeg].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegR].rotation;
-        SourceBones[(int)SourceBoneIndex.rFoot].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_FootR].rotation;
+            SourceBones[(int)SourceBoneIndex.rUpperArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(2).localPosition
+                                                                                        - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(1).localPosition);
+            SourceBones[(int)SourceBoneIndex.rLowerArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmR].GetChild(0).localPosition);
+            SourceBones[(int)SourceBoneIndex.rHand].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_HandR].GetChild(0).localPosition);
 
-        SourceBones[(int)SourceBoneIndex.lUpperLeg].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperLegL].rotation;
-        SourceBones[(int)SourceBoneIndex.lLowerLeg].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegL].rotation;
-        SourceBones[(int)SourceBoneIndex.lFoot].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_FootL].rotation;
+            SourceBones[(int)SourceBoneIndex.lUpperArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(1).localPosition
+                                                                                        - t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].GetChild(2).localPosition);
+            SourceBones[(int)SourceBoneIndex.lLowerArm].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmL].GetChild(0).localPosition);
+            SourceBones[(int)SourceBoneIndex.lHand].orientation = Vector3.Normalize(t_childNodes[(int)Constants.TargetPositionIndex.Cha_HandL].GetChild(0).localPosition);  //target과 source skeleton의 위상이 다르므로, 양 UpperArm은 서로의 좌표를 이용해 구함
 
-        SourceBones[(int)SourceBoneIndex.rUpperArm].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmR].rotation;
-        SourceBones[(int)SourceBoneIndex.rLowerArm].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmR].rotation;
-        SourceBones[(int)SourceBoneIndex.rHand].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_HandR].rotation;
+            //basis 일치를 위해 rotation 값 가져오기
+            rootNode.rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_Hips].rotation;
+            SourceBones[(int)SourceBoneIndex.spine].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_Spine].rotation;
+            SourceBones[(int)SourceBoneIndex.chest].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_Chest].rotation;
+            SourceBones[(int)SourceBoneIndex.neck].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Face].rotation;
+            SourceBones[(int)SourceBoneIndex.head].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Face].rotation;
 
-        SourceBones[(int)SourceBoneIndex.lUpperArm].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmL].rotation;
-        SourceBones[(int)SourceBoneIndex.lLowerArm].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmL].rotation;
-        SourceBones[(int)SourceBoneIndex.lHand].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_HandL].rotation;
+            SourceBones[(int)SourceBoneIndex.rUpperLeg].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperLegR].rotation;
+            SourceBones[(int)SourceBoneIndex.rLowerLeg].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegR].rotation;
+            SourceBones[(int)SourceBoneIndex.rFoot].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_FootR].rotation;
+
+            SourceBones[(int)SourceBoneIndex.lUpperLeg].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperLegL].rotation;
+            SourceBones[(int)SourceBoneIndex.lLowerLeg].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerLegL].rotation;
+            SourceBones[(int)SourceBoneIndex.lFoot].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_FootL].rotation;
+
+            SourceBones[(int)SourceBoneIndex.rUpperArm].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmR].rotation;
+            SourceBones[(int)SourceBoneIndex.rLowerArm].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmR].rotation;
+            SourceBones[(int)SourceBoneIndex.rHand].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_HandR].rotation;
+
+            SourceBones[(int)SourceBoneIndex.lUpperArm].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_UpperArmL].rotation;
+            SourceBones[(int)SourceBoneIndex.lLowerArm].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_LowerArmL].rotation;
+            SourceBones[(int)SourceBoneIndex.lHand].rotation = t_childNodes[(int)Constants.TargetPositionIndex.Cha_HandL].rotation;
+        }
 
 
         //뼈의 길이와 방향 정보를 이용해 각 transform의 position 결정
