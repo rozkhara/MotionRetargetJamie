a53c07f444cdad32e2ce1db3198592e006ebb59d
Rozkhara\media <nytemute@gmail.com>
2022-08-03 10:49:02 +0900

    Add NuGetForUnity and MathNet package

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+public class Test : MonoBehaviour
+{
+    
+
+}
