947886fa2f85ec3c4d25d4caf4257a86ce2f9566
syee19 <syee19@korea.ac.kr>
2022-08-11 15:32:07 +0900

    Temp commit

--- Assets/Scripts/CollisionAvoidance.cs
+++ Assets/Scripts/CollisionAvoidance.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
 
 public class CollisionAvoidance : MonoBehaviour
 {
@@ -21,33 +22,70 @@ public class CollisionAvoidance : MonoBehaviour
         initRotation = new Quaternion[JointCount];
         for(int i=0; i<JointCount; i++)
         {
-            initRotation[i] = Joints[i].rotation;
+            initRotation[i] = Joints[i].localRotation;
         }
 
-        JacobianMatrix(test, Joints[11]);
-        Debug.Log(JacobianA.ToString());
+        test.position = Vector3.Lerp(Joints[26].position, Joints[25].position, 0.5f);
+        //JacobianMatrix(test, Joints[11]);
+        //Debug.Log(JacobianA.ToString());
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //test.position = Joints[11].position;
-        calcTpos(test, Joints[11]);
+        /*
+        
+        Debug.Log(Joints[i].rotation.eulerAngles.ToString());
+        Debug.Log(Joints[i].localRotation.eulerAngles.ToString());
+        Debug.Log(Quaternion.Inverse(Joints[i].localRotation).eulerAngles.ToString());
+        Debug.Log((Quaternion.Inverse(Joints[i].rotation) * Joints[i].parent.rotation).eulerAngles.ToString());
+        Debug.Log((Joints[i].parent.rotation * Quaternion.Inverse(Joints[i].rotation)).eulerAngles.ToString());
+         
+         */
+        int i = 25;
+        var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+        var type = assembly.GetType("UnityEditor.LogEntries");
+        var method = type.GetMethod("Clear");
+        method.Invoke(new object(), null);
+
+        
+        calcTpos(test, Joints[i]);
     }
 
     void calcTpos(Transform pa, Transform pajoint)
     {
         Transform temp = pajoint;
-        Quaternion rot;
-        pa.position = Joints[12].position;
-        /*while (temp != Root)
+        Quaternion r1, deltaLocalRot, rot;
+        int index;
+        pa.position = Vector3.Lerp(Joints[26].position, Joints[25].position, 0.5f);
+        while (temp != Root.parent)
         {
-        }*/
-        Debug.Log(temp.name);
-        rot = temp.parent.rotation * Quaternion.Inverse(temp.rotation);
-        pa.position = rot * (pa.position - temp.position) + temp.position;
-        temp = temp.parent;
+            for(int i=0; i<JointCount; i++)
+            {
+                if(Joints[i] == temp)
+                {
+                    
 
+                    index = i;
+                    r1 = temp.rotation;
+                    deltaLocalRot = temp.localRotation * Quaternion.Inverse(initRotation[index]); //Tpose 기준으로 회전한 localRotation 회전값 -> 이만큼을 localRotation 기준으로 역회전 시켜주어야 함
+                    rot = Quaternion.Inverse(r1) * Quaternion.Inverse(deltaLocalRot) * r1;
+                    pa.position = rot * (pa.position - temp.position) + temp.position;
+
+                    Debug.Log(temp.name);
+                    Debug.LogFormat("initRotation : {0}", (initRotation[i]).eulerAngles.ToString());
+                    Debug.LogFormat("deltaLocalRot : {0}", (deltaLocalRot).eulerAngles.ToString());
+                    Debug.LogFormat("in globalRot : {0}", (rot).eulerAngles.ToString());
+
+                    temp = temp.parent;
+                    break;
+                }
+            }
+        }
+
+        //Debug.Log(temp.name);
         /*
          
         Debug.Log(temp.name);
